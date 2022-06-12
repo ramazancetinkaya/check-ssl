8dd3695d6f342750299b68197fe9c0b08e008475
ramazancetinkaya <75214035+ramazancetinkaya@users.noreply.github.com>
2022-06-12 13:12:31 +0300

    Update ssl.php

--- ssl.php
+++ ssl.php
@@ -1,18 +1,20 @@
 <?php
 
-    function is_ssl() {
-        if ( isset( $_SERVER['HTTPS'] ) ) {
-            if ( 'on' === strtolower( $_SERVER['HTTPS'] ) ) {
-                return true;
-            }
-    
-            if ( '1' == $_SERVER['HTTPS'] ) {
-                return true;
-            }
-        } elseif ( isset( $_SERVER['SERVER_PORT'] ) && ( '443' == $_SERVER['SERVER_PORT'] ) ) {
+function is_ssl() {
+    if (isset($_SERVER['HTTPS'])) {
+        
+        if ('on' === strtolower($_SERVER['HTTPS'])) {
             return true;
         }
-        return false;
+    
+        if ('1' == $_SERVER['HTTPS']) {
+            return true;
+        }
+        
+    } elseif (isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT'])) {
+        return true;
     }
+    return false;
+}
 
 ?>
